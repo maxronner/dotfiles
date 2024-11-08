807ea15699603479ba08dce0ba71193672b8bff0
Max Ronner <max@ronner.dev>
2024-11-08 22:22:54 +0100

    feat: tmux conf

--- install.sh
+++ install.sh
@@ -2,13 +2,36 @@
 
 # ---- Dotfiles ----
 
-# -- Create symlinks --
+# --- Create symlinks ---
 
+# -- Define the dotfiles and their source directory --
+dotfiles=(.gitconfig .tmux.conf .bashrc)
+dotfiles_dir=~/dotfiles
 
+# -- Create symlinks for each file --
+for file in "${dotfiles[@]}"; do
+    # - Check if a symlink already exists and remove it if present -
+    if [ -L ~/"$file" ]; then
+        echo "Removing existing symlink for $file"
+        rm ~/"$file"
+    fi
+
+    # - Check if a regular file or directory exists and back it up if present -
+    if [ -e ~/"$file" ]; then
+        echo "Backing up existing $file to $file.bak"
+        mv ~/"$file" ~/"$file.bak"
+    fi
+
+    # - Create the symlink -
+    ln -s "$dotfiles_dir/$file" ~/"$file"
+    echo "Created symlink for $file"
+done
+
+echo "All specified symlinks created successfully!"
 
 # ---- Setup keychain ----
 
-# -- eza --
+# --- eza ---
 sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
@@ -23,24 +46,26 @@ sudo nala install -y tmux fzf bat tldr thefuck eza xclip
 
 # ---- Program specific setup ----
 
-# -- bat --
+# --- bat ---
 
-# Create the ~/.local/bin directory if it doesn't already exist
+# -- Create the ~/.local/bin directory if it doesn't already exist --
 mkdir -p ~/.local/bin
 
-# Remove the existing symlink (if any) before creating a new one
+# -- Remove the existing symlink (if any) before creating a new one --
 if [ -L ~/.local/bin/bat ]; then
     rm ~/.local/bin/bat
     echo "Removed existing symlink for bat."
 fi
 
-# Create a symlink for bat to ~/.local/bin pointing to /usr/bin/batcat
+# -- Create a symlink for bat to ~/.local/bin pointing to /usr/bin/batcat --
 ln -s /usr/bin/batcat ~/.local/bin/bat
 echo "Created symlink for bat."
 
+# --- tmux ---
 
-# -- tldr --
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Update tldr cache
+# --- tldr ---
+
+# -- Update tldr cache --
 tldr --update
-
