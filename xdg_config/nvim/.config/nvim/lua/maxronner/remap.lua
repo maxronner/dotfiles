c2addff0c8771ebeab94bda2c12e42051e757b08
Max Ronner <max@ronner.dev>
2025-02-02 09:15:17 +0100

    remap changes

--- xdg_config/nvim/.config/nvim/lua/maxronner/remap.lua
+++ xdg_config/nvim/.config/nvim/lua/maxronner/remap.lua
@@ -34,7 +34,7 @@ vim.keymap.set("n", "<C-s>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>X", "<cmd>chmod -x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>d", "<cmd> w !git diff % -<CR>")
+vim.keymap.set("n", "<leader>fd", "<cmd> w !git diff % -<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
