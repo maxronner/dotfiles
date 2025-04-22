439e8119d45a2880cb7a99e80f27fc7f9c06c7af
Max Ronner <max@ronner.dev>
2025-04-22 14:21:39 +0200

    fixed double mapping

--- xdg_config/nvim/.config/nvim/lua/maxronner/lazy/telescope.lua
+++ xdg_config/nvim/.config/nvim/lua/maxronner/lazy/telescope.lua
@@ -19,7 +19,7 @@ return {
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
     vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Telescope: Grep selection" })
-    vim.keymap.set("n", "<leader>fh", builtin.command_history, { desc = "Telescope: Command history" })
+    vim.keymap.set("n", "<leader>fc", builtin.command_history, { desc = "Telescope: Command history" })
     vim.keymap.set("n", "<leader>fH", builtin.search_history, { desc = "Telescope: Search history" })
     vim.keymap.set("n", "<leader>vv", function()
       builtin.find_files { cwd = vim.fn.stdpath "config", prompt_title = "Nvim Config" }
