30c17d63d46ae8ef511a88219529c401f461b0d7
Max Ronner <max@ronner.dev>
2025-04-22 15:15:47 +0200

    nvim terminal

--- xdg_config/nvim/.config/nvim/lua/maxronner/remap.lua
+++ xdg_config/nvim/.config/nvim/lua/maxronner/remap.lua
@@ -40,7 +40,10 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Prev loclist" })
 -- Alternate file (toggle between buffers)
 vim.keymap.set({ "n", "v" }, "<leader>e", "<C-6>", { desc = "Alternate buffer" })
 
+-- Diagnostics to quickfix
 vim.keymap.set('n', '<leader>Q', vim.diagnostic.setqflist, { desc = "Diagnostics to quickfix" })
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Escape terminal mode" })
 
 -- Search & replace word under cursor
 vim.keymap.set("n", "<C-s>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
@@ -74,3 +77,15 @@ vim.keymap.set("n", "<leader>bb", function()
     vim.cmd("normal! ciwtrue")
   end
 end, { silent = true, desc = "Toggle true/false" })
+
+-- Open terminal in bottom split (1/5 screen height)
+vim.keymap.set("n", "<leader>o", function()
+  local total_lines = vim.o.lines
+  local term_height = math.floor(total_lines / 5)
+
+  vim.cmd.new()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, term_height)
+  vim.cmd.startinsert()
+end, { desc = "Open terminal in bottom split (1/5 screen height)" })
