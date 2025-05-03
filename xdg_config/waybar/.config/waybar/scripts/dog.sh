6a7e7e81a6bff990dc43fce9c720d1ffe696273f
Max Ronner <max@ronner.dev>
2025-05-03 16:33:10 +0200

    fixes for laptop

--- xdg_config/waybar/.config/waybar/scripts/dog.sh
+++ xdg_config/waybar/.config/waybar/scripts/dog.sh
@@ -3,7 +3,7 @@ if [[ -n $HA_TOKEN ]]; then
     walk=$(curl -s \
       -H "Authorization: Bearer $HA_TOKEN" \
       -H "Content-Type: application/json" \
-      https://home.ronner.dev/api/states/sensor.walking_dog \ |
+      https://home.ronner.dev/api/states/sensor.walking_dog_digital \ |
     jq '. | .state' | \
     sed 's/"//g')
 fi
