8490e2a17482ed6780dadd8dd2211c001ac22d5b
Max Ronner <max@ronner.dev>
2025-05-15 16:43:48 +0200

    refactor: moved fzf default opts

--- config/environment/dot-config/environment/common-profile.sh
+++ config/environment/dot-config/environment/common-profile.sh
@@ -10,6 +10,17 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 # GTK2
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
 
+if command -v fzf &> /dev/null; then
+    export FZF_DEFAULT_OPTS="
+        --color=fg:#908caa,bg:#232136,hl:#ea9a97
+        --color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
+        --color=border:#44415a,header:#3e8fb0,gutter:#232136
+        --color=spinner:#f6c177,info:#9ccfd8
+        --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa
+        --bind 'ctrl-y:execute-silent(printf {} | cut -f 2- | wl-copy --trim-newline)'"
+    show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
+fi
+
 if [ -d "$HOME/.local/bin" ] ; then
     PATH="$HOME/.local/bin:$PATH"
 fi
