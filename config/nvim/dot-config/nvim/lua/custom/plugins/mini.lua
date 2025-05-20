4b52b45606a20d94e4507474c1fb3f4a8f887eeb
Max Ronner <max@ronner.dev>
2025-05-20 02:23:27 +0200

    feat: added mini.nvim modules

--- config/nvim/dot-config/nvim/lua/custom/plugins/mini.lua
+++ config/nvim/dot-config/nvim/lua/custom/plugins/mini.lua
@@ -6,6 +6,74 @@ return {
       require("mini.surround").setup()
       require("mini.pairs").setup()
       require("mini.comment").setup()
+      require("mini.move").setup()
+      require("mini.operators").setup()
+      require("mini.bracketed").setup()
+      require("mini.trailspace").setup()
+      require("mini.splitjoin").setup()
+      require("mini.starter").setup()
+      require("mini.statusline").setup()
+      require("mini.align").setup()
+      require("mini.jump").setup()
+
+      local miniclue = require('mini.clue')
+      miniclue.setup({
+        triggers = {
+          -- Leader triggers
+          { mode = 'n', keys = '<Leader>' },
+          { mode = 'x', keys = '<Leader>' },
+
+          -- Built-in completion
+          { mode = 'i', keys = '<C-x>' },
+
+          -- `g` key
+          { mode = 'n', keys = 'g' },
+          { mode = 'x', keys = 'g' },
+
+          -- Marks
+          { mode = 'n', keys = "'" },
+          { mode = 'n', keys = '`' },
+          { mode = 'x', keys = "'" },
+          { mode = 'x', keys = '`' },
+
+          -- Registers
+          { mode = 'n', keys = '"' },
+          { mode = 'x', keys = '"' },
+          { mode = 'i', keys = '<C-r>' },
+          { mode = 'c', keys = '<C-r>' },
+
+          -- Window commands
+          { mode = 'n', keys = '<C-w>' },
+
+          -- `z` key
+          { mode = 'n', keys = 'z' },
+          { mode = 'x', keys = 'z' },
+        },
+
+        clues = {
+          -- Enhance this by adding descriptions for <Leader> mapping groups
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+      })
+
+      local map = require("mini.map")
+      require("mini.map").setup({
+        integrations = {
+          map.gen_integration.builtin_search(),
+          map.gen_integration.gitsigns(),
+          map.gen_integration.diagnostic(),
+        },
+        window = {
+          show_integration_count = false,
+          width = 8,
+        },
+      })
+      vim.keymap.set("n", "<leader>m", MiniMap.toggle, { desc = "Toggle MiniMap" })
     end,
   },
 }
