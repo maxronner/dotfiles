8737cb8e9eea8db0abd1e17efb42b02792171b27
Max Ronner <max@ronner.dev>
2025-05-21 21:59:11 +0200

    docs: added desc

--- config/nvim/dot-config/nvim/lua/custom/snippets.lua
+++ config/nvim/dot-config/nvim/lua/custom/snippets.lua
@@ -45,8 +45,8 @@ end
 
 vim.keymap.set({ "i", "s" }, "<c-k>", function()
   return vim.snippet.active { direction = 1 } and vim.snippet.jump(1)
-end, { silent = true })
+end, { silent = true, desc = "Next snippet" })
 
 vim.keymap.set({ "i", "s" }, "<c-j>", function()
   return vim.snippet.active { direction = -1 } and vim.snippet.jump(-1)
-end, { silent = true })
+end, { silent = true, desc = "Previous snippet" })
