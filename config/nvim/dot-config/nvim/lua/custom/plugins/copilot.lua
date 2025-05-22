3a7ff947d9ea71f4f0ecff8acfdc3d80946b65e8
Max Ronner <max@ronner.dev>
2025-05-22 15:01:45 +0200

    feat: codecompanion

--- config/nvim/dot-config/nvim/lua/custom/plugins/copilot.lua
+++ config/nvim/dot-config/nvim/lua/custom/plugins/copilot.lua
@@ -2,5 +2,16 @@ return {
   {
     "github/copilot.vim",
     enabled = false,
-  }
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("codecompanion").setup()
+    end,
+  },
 }
--- config/nvim/dot-config/nvim/plugin/options.lua
+++ config/nvim/dot-config/nvim/plugin/options.lua
@@ -1,6 +1,8 @@
 vim.opt.relativenumber = true
 vim.opt.nu = true
 
+vim.opt.splitright = true
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
