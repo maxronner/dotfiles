057d2c6266d86a5fe2681b42397fec15ad7f854f
Max Ronner <max@ronner.dev>
2025-06-04 23:28:47 +0200

    feat(waybar): add bluetooth power toggle and status indicators
    Introduces a script to toggle bluetooth power on right-click.
    Updates bluetooth module to display distinct icons for on and off states.

---
+++ config/waybar/dot-config/waybar/scripts/bluetooth-toggle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+status=$(bluetoothctl show | grep "Powered:" | awk '{print $2}')
+
+if [[ $status == "yes" ]]; then
+    bluetoothctl power off
+else
+    bluetoothctl power on
+fi
+
