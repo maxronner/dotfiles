f925130a62b9e57caea85b6748d25c4c71a9a897
Max Ronner <max@ronner.dev>
2025-06-10 19:30:01 +0200

    refactor(fzf): centralize tmux pane options
    Define FZF_TMUX_PANE_OPTS for consistent usage across scripts.
    Apply reusable pane options to `tmux-reaper` and `tmux-sessionizer`.
    Use explicit `--tmux --border` for `fzf-emoji`.
    Remove redundant FZF options from `common-profile.sh`.

--- config/environment/dot-config/environment/common-profile.sh
+++ config/environment/dot-config/environment/common-profile.sh
@@ -13,7 +13,6 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
 
 export FZF_DEFAULT_OPTS="
-    --height 40% --tmux bottom,40% --layout reverse --border top
     --color=fg:#908caa,bg:#232136,hl:#ea9a97
     --color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
     --color=border:#44415a,header:#3e8fb0,gutter:#232136
