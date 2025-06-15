349db5e1e79d9b73aa1ec75b3f6c98c3c81aae91
Max Ronner <max@ronner.dev>
2025-06-15 06:34:53 +0200

    refactor(waybar): simplify pacman-updates script output

--- config/waybar/dot-config/waybar/scripts/pacman-updates.sh
+++ config/waybar/dot-config/waybar/scripts/pacman-updates.sh
@@ -4,12 +4,7 @@ updates_output=$(checkupdates)
 status=$?
 
 if [ "$status" -eq 0 ]; then
-    # Updates are available; count and print the lines
     echo "$updates_output" | wc -l
-elif [ "$status" -eq 1 ]; then
-    # No updates are available
+else
     echo "0"
-else # status is 2 or other error
-    # An error occurred (e.g., database locked)
-    echo "?"
 fi
