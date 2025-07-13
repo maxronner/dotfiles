9af4d81d51bcac5af560e69fa7107af1427eab27
Max Ronner <max@ronner.dev>
2025-07-13 16:23:32 +0200

    style(theme): remove transparent background
    Reverts the editor background to be opaque by removing the highlight overrides for `Normal` and `NormalFloat`. This allows the colorscheme's default background color to be used.
    
    Also removes a redundant comment and shortens the description for the yank highlight autocommand.

--- config/nvim/dot-config/nvim/plugin/autocmd.lua
+++ config/nvim/dot-config/nvim/plugin/autocmd.lua
@@ -1,6 +1,5 @@
--- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
+  desc = 'Highlight when yanking text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
--- config/nvim/dot-config/nvim/plugin/colors.lua
+++ config/nvim/dot-config/nvim/plugin/colors.lua
@@ -1,9 +1,6 @@
 local colorscheme = "rose-pine-moon"
 vim.cmd.colorscheme(colorscheme)
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 vim.api.nvim_create_user_command("ReloadTheme", function()
   dofile(vim.fn.stdpath("config") .. "/plugin/colors.lua")
 end, {})
