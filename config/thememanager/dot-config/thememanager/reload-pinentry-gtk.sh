d25d33e9b9eba181bc00ea2c29a9a5d8e95c35d5
Max Ronner <max@ronner.dev>
2025-07-15 19:32:34 +0200

    fix: correct pinentry reload and kmonad config

--- config/thememanager/dot-config/thememanager/reload-pinentry-gtk.sh
+++ config/thememanager/dot-config/thememanager/reload-pinentry-gtk.sh
@@ -3,11 +3,5 @@ source ~/.config/environment.d/gtk.conf
 set +a
 
 systemctl --user import-environment GTK_THEME
-systemctl --user daemon-reexec
-
-while ! systemctl --user is-active gpg-agent.service > /dev/null 2>&1; do
-  sleep 0.1
-done
-
 systemctl --user restart gpg-agent
 
