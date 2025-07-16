917bf2431b4013b4a3a01ff9822dae2d9d48ba63
Max Ronner <max@ronner.dev>
2025-07-16 15:38:10 +0200

    refactor(nvim-autocmd): Use Lua API for trailing whitespace removal
    Rewrote the `BufWritePre` autocmd to remove trailing whitespace using Neovim's Lua API instead of `vim.cmd`. This provides a more robust and idiomatic approach for buffer manipulation.

--- config/nvim/dot-config/nvim/plugin/autocmd.lua
+++ config/nvim/dot-config/nvim/plugin/autocmd.lua
@@ -10,7 +10,21 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   desc = "Remove trailing whitespace",
   pattern = "*",
   callback = function()
-    vim.cmd([[%s/\s\+$//e]])
+    local buf = vim.api.nvim_get_current_buf()
+    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+    local changed = false
+
+    for i, line in ipairs(lines) do
+      local cleaned = line:gsub("%s+$", "")
+      if cleaned ~= line then
+        lines[i] = cleaned
+        changed = true
+      end
+    end
+
+    if changed then
+      vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    end
   end,
 })
 
