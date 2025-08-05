30efad6058bb42794853bb669c8e17033fb52398
Max Ronner <max@ronner.dev>
2025-08-05 12:15:01 +0200

    feat: Add case-insensitive Zsh completion
    This commit configures Zsh completion to be case-insensitive,
    improving usability when completing commands and paths.

--- config/zsh/dot-config/zsh/plugins/tab-highlighting.zsh
+++ config/zsh/dot-config/zsh/plugins/tab-highlighting.zsh
@@ -9,6 +9,8 @@ zstyle ':completion:*' menu select
 
 zstyle ':completion:*' format ''
 
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+
 # Enable colors for completion matches
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
