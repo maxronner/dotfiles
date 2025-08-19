c702ead1fd5fc4368c052940c3c0e5baa23969dc
Max Ronner <max@ronner.dev>
2025-08-20 00:51:37 +0200

    fix: install less bogged

--- install/30-install-yay.sh
+++ install/30-install-yay.sh
@@ -2,7 +2,6 @@
 
 if ! command -v yay > /dev/null; then
     echo "yay not found. Installing yay...";
-    git clone https://aur.archlinux.org/yay.git "${HOME}"/yay && cd "${HOME}"/yay && makepkg -s --noconfirm;
-    sudo pacman -U --noconfirm --needed "$(find "${HOME}"/yay -name "*.pkg.tar.zst" | head -n 1)";
+    git clone https://aur.archlinux.org/yay.git "${HOME}"/yay && cd "${HOME}"/yay && makepkg -si --noconfirm;
     rm -rf "${HOME}"/yay;
 fi
--- install/common.sh
+++ install/common.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 PACKAGE_MANAGER=(sudo pacman -Syu --needed --noconfirm)
-AUR_HELPER=(yay --needed --noconfirm --sudoflags "-S")
+AUR_HELPER=(yay -Syu --needed --noconfirm)
