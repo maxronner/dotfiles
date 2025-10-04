a209d571118ade642ce029f6b3a4640af1f25cc4
Max Ronner <max@ronner.dev>
2025-10-04 03:20:32 +0200

    feat(gtk): Add GTK_IM_MODULE environment variable

--- config/environment/dot-config/environment/app-configs.sh
+++ config/environment/dot-config/environment/app-configs.sh
@@ -1,6 +1,7 @@
 export EDITOR='nvim'
 
 export GTK_THEME=Adwaita:dark
+export GTK_IM_MODULE=simple
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
 
 export PASSWORD_STORE_ENABLE_EXTENSIONS=true
