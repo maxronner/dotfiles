77f7cc18f35465f95a572415b0fba57460e6e996
Max Ronner <max@ronner.dev>
2025-12-10 13:05:36 +0100

    feat(shell-functions): introduce lcount and fix indentation
    Adds a new `lcount` function to count lines of code in files within a given directory (defaults to current) excluding `.git` directories. Results are sorted numerically by line count.

--- config/environment/dot-config/environment/shell-functions.sh
+++ config/environment/dot-config/environment/shell-functions.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
 v() {
-  if command -v tmux >/dev/null 2>&1; then
-    TMUX_SESSION=$(tmux display-message -p '#S')
-    if [ -n "$TMUX_SESSION" ]; then
-      NVIM_SOCKET_DIR="${XDG_RUNTIME_DIR:-/tmp}/nvim-sockets"
-      mkdir -p "$NVIM_SOCKET_DIR"
-      NVIM_LISTEN_ADDR="$NVIM_SOCKET_DIR/nvim-$TMUX_SESSION.sock"
-      nvim --listen "$NVIM_LISTEN_ADDR" "$@"
+    if command -v tmux >/dev/null 2>&1; then
+        TMUX_SESSION=$(tmux display-message -p '#S')
+        if [ -n "$TMUX_SESSION" ]; then
+            NVIM_SOCKET_DIR="${XDG_RUNTIME_DIR:-/tmp}/nvim-sockets"
+            mkdir -p "$NVIM_SOCKET_DIR"
+            NVIM_LISTEN_ADDR="$NVIM_SOCKET_DIR/nvim-$TMUX_SESSION.sock"
+            nvim --listen "$NVIM_LISTEN_ADDR" "$@"
+        else
+            nvim "$@"
+        fi
     else
-      nvim "$@"
+        nvim "$@"
     fi
-  else
-    nvim "$@"
-  fi
 }
 
+lcount() {
+    find "${1:-.}" \
+        -type f \
+        -not -path '*/.git/*' \
+        -exec wc -l {} + |
+        sort -n
+}
