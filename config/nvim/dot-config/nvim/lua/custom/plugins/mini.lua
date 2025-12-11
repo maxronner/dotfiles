2d88c0442197c7e10dc5fecf2f060f4b7f63932b
Max Ronner <max@ronner.dev>
2025-12-11 06:17:57 +0100

    feat(nvim-config): enhance startup experience and plugin integration
    - Add new `mini.starter` items for quick access to Lazy, Mason, and Neovim configuration files.
    - Implement a dynamic footer in `mini.starter` to display Neovim startup time and loaded plugin count.
    - Configure `mini.cmdline` to set an autocomplete delay.

--- config/nvim/dot-config/nvim/lua/custom/plugins/mini.lua
+++ config/nvim/dot-config/nvim/lua/custom/plugins/mini.lua
@@ -3,7 +3,11 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       require("mini.ai").setup()
-      require('mini.cmdline').setup()
+      require('mini.cmdline').setup({
+        autocomplete = {
+          delay = 200,
+        },
+      })
       require("mini.surround").setup()
       require("mini.pairs").setup()
       require("mini.comment").setup()
--- config/nvim/dot-config/nvim/lua/custom/starter.lua
+++ config/nvim/dot-config/nvim/lua/custom/starter.lua
@@ -57,13 +57,35 @@ local starter = require("mini.starter")
 local items = {
   starter.sections.builtin_actions(),
   starter.sections.recent_files(5, true),
-  { name = "Find files", action = "lua require('fzf-lua').files()", section = "Custom" },
-  { name = "Scratch",    action = "ene | setlocal buftype=nofile",  section = "Custom" },
+  { name = "Find files", action = "lua require('fzf-lua').files()",                                  section = "Custom" },
+  { name = "Lazy",       action = "Lazy",                                                            section = "Custom" },
+  { name = "Mason",      action = "Mason",                                                           section = "Custom" },
+  { name = "Config",     action = "lua require('fzf-lua').files({ cwd = '$XDG_CONFIG_HOME/nvim' })", section = "Custom" },
+  { name = "Scratch",    action = "ene | setlocal buftype=nofile",                                   section = "Custom" },
 }
+
+local function get_footer()
+  local stats = require("lazy").stats()
+  local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+  return "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimStarted",
+  callback = function()
+    local stats = require("lazy").stats()
+    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+    local footer = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+    if _G.MiniStarter then
+      MiniStarter.config.footer = footer
+      MiniStarter.refresh()
+    end
+  end,
+})
 
 return {
   evaluate_single = true,
   header = header,
   items = items,
-  footer = "Press <Enter> on an item to launch",
+  footer = get_footer
 }
