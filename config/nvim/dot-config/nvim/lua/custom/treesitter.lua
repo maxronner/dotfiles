b6a1801ac87f1281e51f2e4d31b29c17434b9f38
Max Ronner <max@ronner.dev>
2025-12-16 22:36:49 +0100

    feat(nvim): updated lazy-lock.json and treesitter

--- config/nvim/dot-config/nvim/lua/custom/treesitter.lua
+++ config/nvim/dot-config/nvim/lua/custom/treesitter.lua
@@ -1,125 +1,103 @@
 local M = {}
 
 M.setup = function()
-  ---@diagnostic disable-next-line: missing-fields
-  require("nvim-treesitter.configs").setup({
-    -- A list of parser names, or "all"
+  local group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true })
+
+  require("nvim-treesitter").setup {
     ensure_installed = {
-      "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-      "jsdoc", "bash", "go"
+      "core",
+      "stable",
     },
+  }
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+  local syntax_on = {
+    markdown = true,
+  }
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-    auto_install = true,
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function(args)
+      local bufnr = args.buf
+      local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+      if not ok or not parser then
+        return
+      end
+      pcall(vim.treesitter.start)
 
-    indent = {
-      enable = true
-    },
+      local ft = vim.bo[bufnr].filetype
+      if syntax_on[ft] then
+        vim.bo[bufnr].syntax = "on"
+      end
+    end,
+  })
 
-    highlight = {
-      -- `false` will disable the whole extension
+  require 'nvim-treesitter-textobjects'.setup {
+    lsp_interop = {
       enable = true,
-      disable = function(lang, buf)
-        if lang == "html" then
-          vim.notify(
-            "HTML file, treesitter disabled for performance",
-            vim.log.levels.WARN,
-            { title = "Treesitter" }
-          )
-          return true
-        end
-
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          vim.notify(
-            "File larger than 100KB, treesitter disabled for performance",
-            vim.log.levels.WARN,
-            { title = "Treesitter" }
-          )
-          return true
-        end
-      end,
-
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = { "markdown" },
+      border = 'rounded',
+      floating_preview_opts = {},
+      peek_definition_code = {
+        ["<leader>gf"] = "@function.outer",
+        ["<leader>gF"] = "@class.outer",
+      },
     },
-
-    textobjects = {
-      lsp_interop = {
-        enable = true,
-        border = 'rounded',
-        floating_preview_opts = {},
-        peek_definition_code = {
-          ["<leader>gf"] = "@function.outer",
-          ["<leader>gF"] = "@class.outer",
-        },
+    select = {
+      enable = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
       },
-      select = {
-        enable = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-        },
+    },
+    move = {
+      enable              = true,
+      set_to_root         = true,
+      set_jumps           = true, -- whether to set jumps in the jumplist
+      goto_next_start     = {
+        ["]m"] = "@function.outer",
+        ["]]"] = { query = "@class.outer", desc = "Next class start" },
+        --
+        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+        ["]o"] = "@loop.*",
+        -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+        --
+        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+        ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+        ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
       },
-      move = {
-        enable              = true,
-        set_to_root         = true,
-        set_jumps           = true, -- whether to set jumps in the jumplist
-        goto_next_start     = {
-          ["]m"] = "@function.outer",
-          ["]]"] = { query = "@class.outer", desc = "Next class start" },
-          --
-          -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-          ["]o"] = "@loop.*",
-          -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-          --
-          -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-          -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-          ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
-          ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-        },
-        goto_next_end       = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end   = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-        -- Below will go to either the start or the end, whichever is closer.
-        -- Use if you want more granular movements
-        -- Make it even more gradual by adding multiple queries and regex.
-        goto_next           = {
-          ["]d"] = "@conditional.outer",
-        },
-        goto_previous       = {
-          ["[d"] = "@conditional.outer",
-        }
+      goto_next_end       = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
       },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>ww"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>ws"] = "@parameter.inner",
-        },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end   = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+      -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      goto_next           = {
+        ["]d"] = "@conditional.outer",
+      },
+      goto_previous       = {
+        ["[d"] = "@conditional.outer",
       }
     },
-  })
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>ww"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>ws"] = "@parameter.inner",
+      },
+    }
+  }
 end
 
 return M
