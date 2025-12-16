691b8a78f5f76e7dfea154c5090388b3866a6205
Max Ronner <max@ronner.dev>
2025-12-16 21:44:56 +0100

    chore(deps): Update Neovim plugins
    Updated various plugin dependencies in lazy-lock.json and configured nvim-treesitter-textobjects to explicitly use the 'main' branch.

--- config/nvim/dot-config/nvim/lua/custom/plugins/treesitter.lua
+++ config/nvim/dot-config/nvim/lua/custom/plugins/treesitter.lua
@@ -2,7 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "main",
+    }
   },
   config = function()
     require("custom.treesitter").setup()
