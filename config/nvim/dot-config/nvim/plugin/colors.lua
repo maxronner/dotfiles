64a362c8460f553347cb834f404f97bd7f164d5b
Max Ronner <max@ronner.dev>
2025-12-23 01:30:45 +0100

    refactor(nvim): Config now submodule

--- config/nvim/dot-config/nvim/plugin/colors.lua
+++
@@ -1,6 +0,0 @@
-local colorscheme = "rose-pine-moon"
-vim.cmd.colorscheme(colorscheme)
-
-vim.api.nvim_create_user_command("ReloadTheme", function()
-  dofile(vim.fn.stdpath("config") .. "/plugin/colors.lua")
-end, {})
--- config/nvim/dot-config/nvim/plugin/scratch.lua
+++
@@ -1,8 +0,0 @@
-function OpenScratch()
-  vim.cmd("enew")
-  vim.bo.buftype = "nofile"
-  vim.bo.bufhidden = "hide"
-  vim.bo.swapfile = false
-end
-
-vim.keymap.set("n", "<leader>bs", OpenScratch, { desc = "Open scratch buffer" })
