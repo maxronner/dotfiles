ac689a4c6774812c7f23e93b067ef1f0a7f866ec
Max Ronner <max@ronner.dev>
2025-12-30 22:31:21 +0100

    fix(thememanager): Ensure foot restart is conditional on successful termination
    The `reload-foot.sh` script now uses `&&` instead of separating `pkill foot` and the subsequent launch command. This means a new instance is only executed if an existing instance was successfully killed.

--- config/thememanager/dot-config/thememanager/reload-foot.sh
+++ config/thememanager/dot-config/thememanager/reload-foot.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-pkill foot || true
-swaymsg exec "foot --title=terminal -e zsh -lic tmux-launcher" >/dev/null 2>&1 & disown
+pkill foot && swaymsg exec "foot --title=terminal -e zsh -lic tmux-launcher" >/dev/null 2>&1 & disown
