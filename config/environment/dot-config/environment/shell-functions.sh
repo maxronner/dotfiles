a7bc73afd92cf17ea90d599d4f631ed8128fa884
Max Ronner <max@ronner.dev>
2026-01-04 06:00:00 +0100

    style(sh-func)!: rename rns to del

--- config/environment/dot-config/environment/shell-functions.sh
+++ config/environment/dot-config/environment/shell-functions.sh
@@ -40,7 +40,7 @@ get() {
     yay -Slq | fzf -q "$1" -m --preview 'yay -Si {1}'| xargs -ro yay -S
 
 }
-rns() {
+del() {
     yay -Qq | fzf -q "$1" -m --preview 'yay -Qi {1}' | xargs -ro yay -Rns
 }
 
