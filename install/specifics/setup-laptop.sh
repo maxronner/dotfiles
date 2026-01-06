d799ba16b91f91ef9fc6b2b299632528e064c325
Max Ronner <max@ronner.dev>
2026-01-07 00:46:42 +0100

    fix(install): kmonad uinput access

--- install/specifics/setup-laptop.sh
+++ install/specifics/setup-laptop.sh
@@ -19,7 +19,7 @@ sudo systemctl enable --now NetworkManager.service
 
 info "Allow uinput access for kmonad"
 sudo tee /etc/udev/rules.d/90-uinput.rules <<'EOF'
-KERNEL=="uinput", GROUP="input", MODE="0660"
+KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
 EOF
 
 sudo udevadm control --reload-rules
