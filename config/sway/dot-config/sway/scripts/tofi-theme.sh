f29aef8108c2521278ab0f9bfb8b90c862f9ba9c
Max Ronner <max@ronner.dev>
2026-01-15 22:29:50 +0100

    fix(theme): add missing colon

--- config/sway/dot-config/sway/scripts/tofi-theme.sh
+++ config/sway/dot-config/sway/scripts/tofi-theme.sh
@@ -8,7 +8,7 @@ theme_dir="$HOME/.local/share/themes"
 theme="$(
   find "$theme_dir" -mindepth 1 -maxdepth 1 -print0 \
     | xargs -0 -n1 basename \
-    | tofi --prompt-text theme
+    | tofi --prompt-text theme:
 )" || exit 1
 
 [ -n "$theme" ] || exit 1
