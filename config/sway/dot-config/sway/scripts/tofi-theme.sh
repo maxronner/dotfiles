ea8ebc11807ca1acd7995e4ca9217aec7c882aed
Max Ronner <max@ronner.dev>
2026-01-15 22:19:22 +0100

    feat: Add functionality to change Tofi theme via Sway config
    Introduces a new script to select and apply a Tofi theme using the `thememanager`.
    A new keybinding `$mod+o` is added in the Sway configuration to execute this script.

---
+++ config/sway/dot-config/sway/scripts/tofi-theme.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script is used to change the theme of the system.
+# It is called by the tofi command-line tool.
+
+theme_dir="$HOME/.local/share/themes"
+
+theme="$(
+  find "$theme_dir" -mindepth 1 -maxdepth 1 -print0 \
+    | xargs -0 -n1 basename \
+    | tofi --prompt-text theme
+)" || exit 1
+
+[ -n "$theme" ] || exit 1
+thememanager set "$theme"
+
