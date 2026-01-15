24655088eb345b768ba74e222b262343647d99ee
Max Ronner <max@ronner.dev>
2026-01-15 20:16:14 +0100

    feat(theme): add kanagawa theme

---
+++ config/thememanager/dot-local/share/themes/kanagawa/fzf/profile.sh
@@ -0,0 +1,6 @@
+export FZF_COLOR_OPTS="
+    --color=fg:#dcd7ba,bg:#1f1f28,hl:#7e9cd8
+    --color=fg+:#dcd7ba,bg+:#2a2a37,hl+:#7e9cd8
+    --color=border:#54546d,header:#957fb8,gutter:#1f1f28
+    --color=spinner:#98bb6c,info:#7aa89f
+    --color=pointer:#e46876,marker:#e6c384,prompt:#c8c093"
