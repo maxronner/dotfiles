f119bdd424b8e88b81f45855475911f0e00dfc4e
Max Ronner <max@ronner.dev>
2026-01-16 11:32:47 +0100

    feat(config): Sort themes in tofi theme selector

--- config/sway/dot-config/sway/scripts/tofi-theme.sh
+++ config/sway/dot-config/sway/scripts/tofi-theme.sh
@@ -7,6 +7,7 @@ theme_dir="$HOME/.local/share/themes"
 
 theme="$(
   find "$theme_dir" -mindepth 1 -maxdepth 1 -print0 \
+    | sort -z \
     | xargs -0 -n1 basename \
     | tofi --prompt-text theme:
 )" || exit 1
