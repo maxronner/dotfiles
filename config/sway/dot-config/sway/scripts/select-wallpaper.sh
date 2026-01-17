c978c3f335dc95c46ed803b0cb53eb69c7e9d756
Max Ronner <max@ronner.dev>
2026-01-18 00:01:06 +0100

    feat: random list

--- config/sway/dot-config/sway/scripts/select-wallpaper.sh
+++ config/sway/dot-config/sway/scripts/select-wallpaper.sh
@@ -3,6 +3,7 @@
 wallpaper_dir="${WALLPAPER_DIR:-$HOME/.local/share/wallpapers}"
 wallpaper="$(
   find -L "$wallpaper_dir" -mindepth 1 -maxdepth 1 -type f -printf '%f\n' \
+    | sort -R \
     | tofi --prompt-text "wallpaper: "
 )"
 [ -n "$wallpaper" ] || exit 1
