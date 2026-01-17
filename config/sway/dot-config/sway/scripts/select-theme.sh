fb93b959682384b8a96ad4ae81fc56cc09d94802
Max Ronner <max@ronner.dev>
2026-01-17 17:59:57 +0100

    feat: Use theme manager for theme selection
    Replaces manual directory finding and sorting of themes in `select-theme.sh` with a call to `thememanager list` for better theme integration and listing.

--- config/sway/dot-config/sway/scripts/select-theme.sh
+++ config/sway/dot-config/sway/scripts/select-theme.sh
@@ -2,9 +2,7 @@
 
 theme_dir="$HOME/.local/share/themes"
 theme="$(
-  find "$theme_dir" -mindepth 1 -maxdepth 1 -print0 \
-    | sort -z \
-    | xargs -0 -n1 basename \
+  thememanager list \
     | tofi --prompt-text "theme: "
 )" || exit 1
 
