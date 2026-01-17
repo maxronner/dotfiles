35f0df4171539ed4e9e1b9a0cfafc82541783abd
Max Ronner <max@ronner.dev>
2026-01-17 17:52:04 +0100

    feat: Improve robustness of pinentry-gtk reload script

--- config/thememanager/dot-config/thememanager/reload-pinentry-gtk.sh
+++ config/thememanager/dot-config/thememanager/reload-pinentry-gtk.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+
+gtk_conf_file="$HOME/.config/environment.d/gtk.conf"
+
+if [[ ! -f "$gtk_conf_file" ]]; then
+  echo "GTK configuration file not found. Exiting."
+  exit
+fi
+
 set -a
-source ~/.config/environment.d/gtk.conf
+source "$gtk_conf_file"
 set +a
 
 systemctl --user import-environment GTK_THEME
