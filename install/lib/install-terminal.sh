f9bc2582d8e5fb2cf1d742e25b56bc62c933c6f8
Max Ronner <max@ronner.dev>
2026-04-10 06:21:59 +0200

    feat: add system/packages.sh phase, remove HOME mutations from install-terminal

--- install/lib/install-terminal.sh
+++ install/lib/install-terminal.sh
@@ -10,9 +10,3 @@ ENV="${1:-}"
 collect_pkg_entries repo "$ENV" PKGS
 
 install_repo_packages "${PKGS[@]}"
-
-info "Symlinking vim-spell-sv to $HOME/.local/share/nvim/site/spell"
-mkdir -p ~/.local/share/nvim/site/spell
-stow -d /usr/share/vim/vimfiles -t "$HOME/.local/share/nvim/site/spell" spell
-
-mkdir -p ~/.local/share/zsh
